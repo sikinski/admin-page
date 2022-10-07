ebd84c94b5cadb1cce08776c3bdd2bb1e175e833
sikinski <ulyana.sikinski@gmail.com>
2022-10-08 00:14:28 +0500

    added title in tg-channels-view

--- src/views/TGChannelsView.vue
+++ src/views/TGChannelsView.vue
@@ -1,9 +1,30 @@
 <template>
-  <div id="tg-channels" class="view flex-center">TG</div>
+  <div id="tg-channels" class="view flex-center">
+    <div class="having-groups">
+      <p class="title">Реклама в группах</p>
+      <p>Here will be a real table from google sheets API</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style lang="sass">
+#tg-channels
+  .having-groups
+    .title
+    .table
+      display: block
+      border-spacing: 0
+      thead
+        background-color: green
+        tr
+          th
+            padding: 14px 27px
+      tbody
+        tr
+          td
+            border: 1px solid red
+</style>
